63f38e32ed16af560c4c3d289d70bb4dd58399f2
jacksm1221 <jacksm1221@gmail.com>
2026-01-12 15:54:01 -0800

    Create 09_30Days.py

---
+++ 09_30Days.py
@@ -0,0 +1,18 @@
+##Q1
+# age = input("Enter your age: ")
+# if int(age) >= 18:
+#     print("You are old enough to drive.")
+# else:
+#     print(f"You need {18 - int(age)} more years to drive.")
+
+##Q2
+# my_age = 32
+# your_age = input("Enter your age: ")
+# if my_age > int(your_age):
+#     print(f"I am {my_age - int(your_age)} years older than you.")
+# elif my_age < int(your_age):
+#     print(f"I am {int(your_age) - my_age} years younger than you.")
+# else:
+#     print("We are the same age.")
+
+##Q3
