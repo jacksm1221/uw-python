33c94d3b4329bb81f6a80ca92fff6bee14b4edec
jacksm1221 <jacksm1221@gmail.com>
2026-01-15 14:37:50 -0800

    Completed Day 9

--- 09_30Days.py
+++ 09_30Days.py
@@ -50,14 +50,63 @@
 #     print("F")
 
 ##Q2
-month = input("What month is it? ")
-if month in ("September, October, November"):
-    print("Autumn")
-elif month in ("December, January, February"):
-    print("Winter")
-elif month in ("March, April, Mary"):
-    print("Spring")
-elif month in ("June, July, August"):
-    print("Summer")
+# month = input("What month is it? ")
+# if month in ("September, October, November"):
+#     print("Autumn")
+# elif month in ("December, January, February"):
+#     print("Winter")
+# elif month in ("March, April, Mary"):
+#     print("Spring")
+# elif month in ("June, July, August"):
+#     print("Summer")
+# else:
+#     print("That is not a month!")
+
+##Q3
+# fruits = ['banana', 'orange', 'mango', 'lemon']
+# newFruit = input("Can you name me a fruit? ")
+# if newFruit not in fruits:
+#     fruits.append(newFruit)
+#     print(fruits)
+# else:
+#     print("That fruit already exists in my list." + str(fruits))
+
+### Level 3
+##Q1
+
+#  * Check if the person dictionary has skills key, if so print out the middle skill in the skills list.
+#  * Check if the person dictionary has skills key, if so check if the person has 'Python' skill and print out the result.
+#  * If a person skills has only JavaScript and React, print('He is a front end developer'), if the person skills has Node, Python, MongoDB, print('He is a backend developer'), if the person skills has React, Node and MongoDB, Print('He is a fullstack developer'), else print('unknown title') - for more accurate results more conditions can be nested!
+#  * If the person is married and if he lives in Finland, print the information in the following format:
+person = {
+    'first_name': 'Asabeneh',
+    'last_name': 'Yetayeh',
+    'age': 250,
+    'country': 'Finland',
+    'is_married': True,
+    'skills': 
+    # ['JavaScript', 'React']
+    ['Node', 'Python', 'MongoDB']
+    # ['React', 'Node', 'MongoDB']
+    # {'Node', 'MongoDB', 'Python'} "Trying for unordered."
+,
+    'address': {
+        'street': 'Space street',
+        'zipcode': '02210'
+        }
+    }
+
+if 'skills' in person.keys():
+    print(person['skills'][round(len((person['skills']))/2)])
+    if 'Python' in person['skills']:
+        print("Person has skills in Python.")
+if person['skills'] == ['JavaScript', 'React']:
+    print('He is a front end developer.')
+elif ['Node', 'Python', 'MongoDB'] == person['skills']:
+    print('He is a backend developer.')
+elif ['React', 'Node', 'MongoDB'] == person['skills']:
+    print('He is a fullstack developer.')
 else:
-    print("That is not a month!")
+    print('unknown title')
+if person['is_married'] == True and person['country'] == 'Finland':
+    print('Asabeneh Yetayeh lives in Finland. He is married.')
