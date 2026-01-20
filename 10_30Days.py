3380747716a95ea3a89c4d1ad7e7c34a4ccf9f53
jacksm1221 <jacksm1221@gmail.com>
2026-01-20 12:50:52 -0800

    Completed Level 1-Q2

--- 10_30Days.py
+++ 10_30Days.py
@@ -1,2 +1,30 @@
 ### Level 1
 ##Q1
+#For Loop 0-10
+# for number in range(11):
+#     print(number)
+#     number += 1
+
+#While Loop 0-10
+# number = 0
+# while number in range(11):
+#     print(number)
+#     number += 1
+
+##Q2
+#For Loop 10-0
+# for number in range(11,0):
+#     print(number)
+#     number -= 1
+# else:
+#     print(f'The loop ended at {number}.')
+# for number in range(10,-1,-1):
+#     print(number)
+# else:
+#     print('The loop stops at', number)
+
+# #While Loop 10-0
+# number = 10
+# while number > -1 :
+#     print(number)
+#     number -= 1
