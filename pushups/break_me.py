bcb785bc3d8f290248704bf96aa717572c5c5399
jacksm1221 <jacksm1221@gmail.com>
2026-02-04 13:51:24 -0800

    Completed 1/2 Exception Errors

---
+++ pushups/break_me.py
@@ -0,0 +1,10 @@
+# def NameError(): #indicates that you have tried to use a symbol that is not bound to a value.
+#     print(chicken)
+# NameError()
+
+# def TypeError(): #indicates that you have tried to use the wrong kind of object for an operation.
+#     print(4 + "hello")
+# TypeError()
+
+# SyntaxError():
+# AttributeError():
