dd88e6e08b7aa11c03475bc81d66737e7b724146
jacksm1221 <jacksm1221@gmail.com>
2026-02-04 14:07:23 -0800

    Completed all exception errors

--- pushups/break_me.py
+++ pushups/break_me.py
@@ -6,5 +6,11 @@
 #     print(4 + "hello")
 # TypeError()
 
-# SyntaxError():
-# AttributeError():
+# def SyntaxError() #indicates that you have mis-typed something.
+#     print("I won't print due to a syntax error!")
+# SyntaxError()
+
+# def AttributeError(): #indicates that you have tried to access an attribute or method that an object does not have. (This often means you have a different type of object than you expect.)
+#     chicken = 2
+#     chicken.pop()
+# AttributeError()
