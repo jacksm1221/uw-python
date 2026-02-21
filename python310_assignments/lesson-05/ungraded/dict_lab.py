cf00d1a0da8189c120f8918f5a4c578136c0e17e
jacksm1221 <jacksm1221@gmail.com>
2026-02-20 16:19:20 -0800

    Completed dict_lab P1

---
+++ python310_assignments/lesson-05/ungraded/dict_lab.py
@@ -0,0 +1,14 @@
+d = {
+    "name": "Chris",
+    "city": "Seattle",
+    "cake": "chocolate"
+}
+
+# Part 1
+print(d)
+d.pop("cake")
+print(d)
+d.update({"fruit": "mango"})
+print("mango" in d.values())
+
+# Part 2
