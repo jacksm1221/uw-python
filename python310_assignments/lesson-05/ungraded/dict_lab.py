6adf13ec8929f2bce0cc4cff929463604feac518
jacksm1221 <jacksm1221@gmail.com>
2026-02-21 19:04:03 -0800

    Completed P3 of dict_lab

--- python310_assignments/lesson-05/ungraded/dict_lab.py
+++ python310_assignments/lesson-05/ungraded/dict_lab.py
@@ -16,3 +16,33 @@ d2 = {}
 for key, value in d.items():
     d2.update({key: value.count("t")})
 print(d2)
+
+# Part 3
+def create_sets(display=None):
+    s2 = []
+    s3 = []
+    s4 = []
+    for num in range(1,21):
+        if num % 2 == 0:
+            s2.append(num)
+        if num % 3 == 0:
+            s3.append(num)
+        if num % 4 == 0:
+            s4.append(num)
+    s2 = set(s2)
+    s3 = set(s3)
+    s4 = set(s4)
+    if not display:
+        print(s2)
+        print(s3)
+        print(s4)
+    if s3 <= s2:
+        print(s2)
+        print(s3)
+        print(s4)
+    if s4 <= s2:
+        print(s2)
+        print(s3)
+        print(s4)
+
+create_sets()
